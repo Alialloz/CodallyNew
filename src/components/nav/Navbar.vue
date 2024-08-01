b49cd72811c0fd5a0d181ee1af8315388ba1768d
julien <jblancon@enssat.fr>
2024-08-01 18:16:59 +0200

    correcetion du script i18n

--- src/components/nav/Navbar.vue
+++ src/components/nav/Navbar.vue
@@ -1,50 +1,25 @@
 <template>
-  <header
-    class="main-header w-100"
-    :class="[darkNav ? 'position-absolute' : '', classOption]"
-  >
-    <nav
-      class="navbar navbar-expand-xl sticky-header z-10"
-      :class="[
-        darkNav ? 'navbar-dark' : 'navbar-light',
-        isSticky ? 'affix' : '',
-      ]"
-    >
-      <div
-        class="container d-flex align-items-center justify-content-lg-between position-relative"
-      >
-        <router-link
-          to="/"
-          class="navbar-brand d-flex align-items-center mb-md-0 text-decoration-none"
-        >
-          <img
-            src="@/assets/img/logo-white.png"
-            alt="Logo"
-            class="img-fluid logo-white"
-          />
-          <img
-            src="logoAssets\logoCodallyFondTransparent.png"
-            alt="Logo de l'entreprise codally"
-            class="img-fluid logo-color"
-          />
+  <header class="main-header w-100" :class="[darkNav ? 'position-absolute' : '', classOption]">
+    <nav class="navbar navbar-expand-xl sticky-header z-10" :class="[
+      darkNav ? 'navbar-dark' : 'navbar-light',
+      isSticky ? 'affix' : '',
+    ]">
+      <div class="container d-flex align-items-center justify-content-lg-between position-relative">
+        <router-link to="/" class="navbar-brand d-flex align-items-center mb-md-0 text-decoration-none">
+          <img src="@/assets/img/logo-white.png" alt="Logo" class="img-fluid logo-white" />
+          <img src="logoAssets\logoCodallyFondTransparent.png" alt="Logo de l'entreprise codally"
+            class="img-fluid logo-color" />
         </router-link>
 
-        <router-link
-          class="navbar-toggler position-absolute right-0 border-0"
-          to="#offcanvasWithBackdrop"
-          role="button"
-        >
-          <span
-            class="far fa-bars"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasWithBackdrop"
-            aria-controls="offcanvasWithBackdrop"
-          ></span>
+        <router-link class="navbar-toggler position-absolute right-0 border-0" to="#offcanvasWithBackdrop"
+          role="button">
+          <span class="far fa-bars" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop"
+            aria-controls="offcanvasWithBackdrop"></span>
         </router-link>
 
         <div class="collapse navbar-collapse justify-content-center">
           <ul class="nav col-12 col-md-auto justify-content-center main-menu">
-           <!-- <li class="nav-item dropdown">
+            <!-- <li class="nav-item dropdown">
               <a
                 class="nav-link dropdown-toggle"
                 href="#"
@@ -224,14 +199,10 @@
               </div>
             </li> -->
             <li>
-              <router-link to="/" class="nav-link"
-                >Home</router-link
-              >
+              <router-link to="/" class="nav-link">Home</router-link>
             </li>
             <li>
-              <router-link to="/services" class="nav-link"
-                >Services</router-link
-              >
+              <router-link to="/services" class="nav-link">Services</router-link>
             </li>
             <li>
               <router-link to="/about-us" class="nav-link">Our projects</router-link>
@@ -240,30 +211,17 @@
               <router-link to="/contact" class="nav-link">Contact Us</router-link>
             </li>
             <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                >Pages</a
-              >
-              <div
-                class="dropdown-menu border-0 rounded-custom shadow py-0 bg-white"
-              >
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">Pages</a>
+              <div class="dropdown-menu border-0 rounded-custom shadow py-0 bg-white">
                 <div class="dropdown-grid rounded-custom width-full">
                   <div class="dropdown-grid-item">
                     <h6 class="drop-heading">Useful Links</h6>
                     <router-link to="/contact" class="dropdown-link px-0">
-                      <span class="me-2"
-                        ><i class="far fa-address-book"></i
-                      ></span>
+                      <span class="me-2"><i class="far fa-address-book"></i></span>
                       <div class="drop-title">Contact Us</div>
                     </router-link>
-                    <router-link
-                      to="/service-single"
-                      class="dropdown-link px-0"
-                    >
+                    <router-link to="/service-single" class="dropdown-link px-0">
                       <span class="me-2"><i class="far fa-server"></i></span>
                       <div class="drop-title">Services Single</div>
                     </router-link>
@@ -276,30 +234,19 @@
                       <div class="drop-title">News Details</div>
                     </router-link>
                     <router-link to="/career" class="dropdown-link px-0">
-                      <span class="me-2"
-                        ><i class="far fa-graduation-cap"></i
-                      ></span>
+                      <span class="me-2"><i class="far fa-graduation-cap"></i></span>
                       <div class="drop-title">Career</div>
                     </router-link>
                     <router-link to="/career-single" class="dropdown-link px-0">
-                      <span class="me-2"
-                        ><i class="far fa-user-graduate"></i
-                      ></span>
+                      <span class="me-2"><i class="far fa-user-graduate"></i></span>
                       <div class="drop-title">Career Single</div>
                     </router-link>
                     <router-link to="/integrations" class="dropdown-link">
-                      <span class="me-2"
-                        ><i class="far fa-rocket-launch"></i
-                      ></span>
+                      <span class="me-2"><i class="far fa-rocket-launch"></i></span>
                       <div class="drop-title">Integrations</div>
                     </router-link>
-                    <router-link
-                      to="/integration-single"
-                      class="dropdown-link px-0"
-                    >
-                      <span class="me-2"
-                        ><i class="far fa-paper-plane"></i
-                      ></span>
+                    <router-link to="/integration-single" class="dropdown-link px-0">
+                      <span class="me-2"><i class="far fa-paper-plane"></i></span>
                       <div class="drop-title">Integration Single</div>
                     </router-link>
                   </div>
@@ -314,9 +261,7 @@
                       <div class="drop-title">Help Details</div>
                     </router-link>
                     <router-link to="/request-for-demo" class="dropdown-link">
-                      <span class="me-2"
-                        ><i class="far fa-laptop-code"></i
-                      ></span>
+                      <span class="me-2"><i class="far fa-laptop-code"></i></span>
                       <div class="drop-title">Request for Demo</div>
                     </router-link>
                     <router-link to="/login" class="dropdown-link">
@@ -328,15 +273,11 @@
                       <div class="drop-title">User SignUp</div>
                     </router-link>
                     <router-link to="/recovery" class="dropdown-link">
-                      <span class="me-2"
-                        ><i class="far fa-trash-undo"></i
-                      ></span>
+                      <span class="me-2"><i class="far fa-trash-undo"></i></span>
                       <div class="drop-title">Recovery Account</div>
                     </router-link>
                     <router-link to="/notfound" class="dropdown-link">
-                      <span class="me-2"
-                        ><i class="far fa-exclamation-triangle"></i
-                      ></span>
+                      <span class="me-2"><i class="far fa-exclamation-triangle"></i></span>
                       <div class="drop-title">404 Page</div>
                     </router-link>
                     <router-link to="/coming-soon" class="dropdown-link">
@@ -349,17 +290,9 @@
             </li>
           </ul>
         </div>
-        <div
-          class="action-btns text-end me-5 me-lg-0 d-none d-md-block d-lg-block"
-        >
-          <router-link
-            to="/login"
-            class="btn btn-link text-decoration-none me-2"
-            >Sign In</router-link
-          >
-          <router-link to="/request-for-demo" class="btn btn-primary"
-            >Get Started</router-link
-          >
+        <div class="action-btns text-end me-5 me-lg-0 d-none d-md-block d-lg-block">
+          <router-link to="/login" class="btn btn-link text-decoration-none me-2">Sign In</router-link>
+          <router-link to="/request-for-demo" class="btn btn-primary">Get Started</router-link>
         </div>
         <div class="language-switcher ms-3">
           <select @change="changeLanguage($event)">
@@ -374,7 +307,9 @@
 </template>
 
 <script>
+import { useI18n } from 'vue-i18n';
 import Offcanvas from './Offcanvas.vue';
+
 export default {
   components: { Offcanvas },
   name: 'Navbar',
@@ -409,5 +344,5 @@ export default {
     };
   },
 };
-
 </script>
+
