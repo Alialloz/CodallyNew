017274969f889b06bf0e32f2ed93e37aa0f0211e
julien <jblancon@enssat.fr>
2024-08-02 12:53:10 +0200

    add translation

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,8 @@ import { createI18n } from 'vue-i18n';
 
 const messages = {
   en: {
+    service: "Services",
     Home :  "Home",
-<<<<<<< HEAD
     slogan_1 : "Turn your dreams into technological realities",
     slogan_2 : "#1 Software Solution for Your Business",
     description : "We are a technology consulting firm specializing in the development of innovative and customized diverse software solutions",
@@ -12,30 +12,13 @@ const messages = {
     about : "About Us",
   },
   fr: {
-    about: "À propos de nous",
-    service: "Service",
+    service: "Services",
     Home :  "Acceuill",
     slogan_1 : "Transformez vos rêves en réalités technologiques",
     slogan_2 :"#1 Solution Logicielle pour Votre Entreprise",
     description : "Nous sommes une firme de conseil technologique spécialisée dans le développement de solutions logicielles variées innovantes et personnalisées.",
     contact : "Contactez-nous",
     about : "À propos de nous",
-=======
-    service : "Service",
-    Project : "our project",
-    about : "About Us",
-    slogan : "Turn your dreams into technological realities",
-    test : "#1 Software Company In World"
-  },
-  fr: {
-    Home :  "Accueil",
-    service : "Services",
-    Project : "Nos Projects",
-    about : "À propos de nous",
-    slogan : "Transformez vos rêves en réalités technologiques",
-    test : "#1 companie de logiciels au monde"
-
->>>>>>> 81b5bb293d31f64d83b7f37768cc20264fcb9254
   },
 };
 
