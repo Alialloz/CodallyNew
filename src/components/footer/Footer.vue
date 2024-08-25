471f16d54f1573d80a0af0471879b0df2c8322f6
julien <j.blancon1206@gmail.com>
2024-08-25 17:31:24 +0200

    add social netwrok icon and link for facebook linkedin only into herofive and both footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -126,10 +126,19 @@
             <div class="footer-single-col text-start text-lg-end text-md-end">
               <ul class="list-unstyled list-inline footer-social-list mb-0">
                 <li class="list-inline-item">
+                  <a href="#"><i class="fab fa-github"></i></a>
+                </li>
+                <li class="list-inline-item">
                   <a href="https://www.facebook.com/people/CodAlly/61556088625053/?is_tour_dismissed=true"><i class="fab fa-facebook-f"></i></a>
                 </li>
                 <li class="list-inline-item">
                   <a href="https://ca.linkedin.com/company/codally"><i class="fab fa-linkedin"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#"><i class="fab fa-twitter"></i></a>
                 </li>
               </ul>
             </div>
--- src/components/hero/HeroFive.vue
+++ src/components/hero/HeroFive.vue
@@ -24,11 +24,20 @@
           </div>
           <div class="row justify-content-lg-start mt-60">
             <ul class="list-unstyled list-inline footer-social-list mb-0">
+              <li class="list-inline-item">
+                  <a href="#"><i class="fab fa-github"></i></a>
+                </li>
                 <li class="list-inline-item">
                   <a href="https://www.facebook.com/people/CodAlly/61556088625053/?is_tour_dismissed=true"><i class="fab fa-facebook-f"></i></a>
                 </li>
                 <li class="list-inline-item">
                   <a href="https://ca.linkedin.com/company/codally"><i class="fab fa-linkedin"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#"><i class="fab fa-twitter"></i></a>
                 </li>
               </ul>
           </div>
