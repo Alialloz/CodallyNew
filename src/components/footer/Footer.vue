7d382cfd47b6cd96d8b12cb900349781a90d4539
julien <j.blancon1206@gmail.com>
2024-08-28 17:18:41 +0200

    add content in footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -12,10 +12,10 @@
                   class="img-fluid logo-white" />
                 <img src="@/assets/img/logo-color.png" alt="logo" class="img-fluid logo-color" />
                 <!-- Texte sous le logo -->
-              <p class="mt-2 text-white">
-                &nbsp&nbsp{{ $t('Souscription') }}
-              </p>
-              <!-- Fin du texte sous le logo -->
+                <p class="mt-2 text-white">
+                  &nbsp&nbsp{{ $t('Souscription') }}
+                </p>
+                <!-- Fin du texte sous le logo -->
               </div>
             </div>
           </div>
@@ -54,21 +54,15 @@
                     <li>
                       <router-link to="/contact-us" class="text-decoration-none">{{ $t('contact') }}</router-link>
                     </li>
-                    
+                    <li class="contact-item">
+                      <i class="fas fa-phone-alt"></i> <!-- Icône téléphone -->
+                      <a href="tel:(514) 600-8289">(514) 600-8289</a>
+                    </li>
+                    <li class="contact-item">
+                      <i class="fas fa-envelope"></i> <!-- Icône email -->
+                      <a href="mailto:codally.agency@outlook.com">codally.agency@outlook.com</a>
+                    </li>
                   </ul>
-                
-                <ul class="list-unstyled contact-list mb-lg-0 text-muted">
-                  <li class="contact-item">
-                    <i class="fas fa-phone-alt"></i> <!-- Icône téléphone -->
-                    <span>(514) 600-8289</span>
-                  </li>
-                  <li class="contact-item">
-                    <i class="fas fa-envelope"></i> <!-- Icône email -->
-                    <span>codally.agency@outlook.com</span>
-                  </li>
-                </ul>
-
-                  
                 </div>
               </div>
             </div>
@@ -77,47 +71,47 @@
         </div>
       </div>
     </div>
-  <!--footer top end-->
+    <!--footer top end-->
 
-  <!--footer bottom start-->
-  <div class="footer-bottom text-white py-4" :class="footerDark ? 'bg-dark bg-gradient' : 'footer-light'">
-    <div class="container">
-      <div class="row justify-content-between align-items-center">
-        <div class="col-md-7 col-lg-7">
-          <div class="copyright-text">
-            <p class="mb-lg-0 mb-md-0">
-              &copy; 2022 Quiety Rights Reserved. Designed By
-              <a href="https://themetags.com/" class="text-decoration-none">ThemeTags</a>
-            </p>
+    <!--footer bottom start-->
+    <div class="footer-bottom text-white py-4" :class="footerDark ? 'bg-dark bg-gradient' : 'footer-light'">
+      <div class="container">
+        <div class="row justify-content-between align-items-center">
+          <div class="col-md-7 col-lg-7">
+            <div class="copyright-text">
+              <p class="mb-lg-0 mb-md-0">
+                &copy; 2022 Quiety Rights Reserved. Designed By
+                <a href="https://themetags.com/" class="text-decoration-none">ThemeTags</a>
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="col-md-4 col-lg-4">
-          <div class="footer-single-col text-start text-lg-end text-md-end">
-            <ul class="list-unstyled list-inline footer-social-list mb-0">
-              <li class="list-inline-item">
-                <a href="https://github.com/CodAlly-Agency"><i class="fab fa-github"></i></a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://www.facebook.com/people/CodAlly/61556088625053/?is_tour_dismissed=true"><i
-                    class="fab fa-facebook-f"></i></a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://ca.linkedin.com/company/codally"><i class="fab fa-linkedin"></i></a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://x.com/CodAllyAgency"><i class="fab fa-twitter"></i></a>
-              </li>
-            </ul>
+          <div class="col-md-4 col-lg-4">
+            <div class="footer-single-col text-start text-lg-end text-md-end">
+              <ul class="list-unstyled list-inline footer-social-list mb-0">
+                <li class="list-inline-item">
+                  <a href="https://github.com/CodAlly-Agency"><i class="fab fa-github"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="https://www.facebook.com/people/CodAlly/61556088625053/?is_tour_dismissed=true"><i
+                      class="fab fa-facebook-f"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="https://ca.linkedin.com/company/codally"><i class="fab fa-linkedin"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="https://x.com/CodAllyAgency"><i class="fab fa-twitter"></i></a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <!--footer bottom end-->
-</footer>
+    <!--footer bottom end-->
+  </footer>
 
-    <!-- Bouton pour remonter en haut de la page -->
-    <button @click="scrollToTop" class="scroll-to-top-btn">↑</button>
+  <!-- Bouton pour remonter en haut de la page -->
+  <button @click="scrollToTop" class="scroll-to-top-btn">↑</button>
 </template>
 
 <script>
@@ -162,30 +156,37 @@ export default {
 
 
 <style>
-  .footer-single-col .text-muted {
-    color: #888; /* Couleur grise similaire aux autres textes sous les colonnes */
-  }
+.contact-item {
+  font-size: 15px;
+  margin: 10px 0;
+  display: flex;
+  opacity: 0.7;
+  font-weight: 500;
+  font-family: "Poppins", sans-serif;
+  transition: all 0.3s ease-in-out;
+  align-items: center;
+}
 
-  .link-highlight {
-    color: #888; /* Texte en blanc */
-    text-decoration: underline; /* Surlignement */
-  }
+.contact-item:hover {
+  opacity: 1;
+}
 
-  .contact-item {
-    display: flex;
-    align-items: center;
-    color: #888; /* Gris similaire aux autres textes */
-    margin-bottom: 10px;
-  }
+.contact-item i {
+  margin-right: 8px;
+  /* Espace entre l'icône et le texte */
+  font-size: 16px;
+  color: #ffffff;
+  /* Icônes en blanc pour contraste */
+}
 
-  .contact-item i {
-    margin-right: 8px; /* Espace entre l'icône et le texte */
-    font-size: 16px;
-    color: #fff; /* Icônes en blanc pour contraste */
-  }
+.contact-item a, .contact-item span {
+  display: inline-block;
+  text-decoration: none; /* Ensures the link doesn't have an underline */
+  color: inherit; /* Inherits color from the parent, ensuring consistency */
+}
 
-  .footer-nav-list li {
-    margin-bottom: 8px; /* Ajustement de l'espace entre les items */
-  }
-
+.footer-nav-list li {
+  margin-bottom: 8px;
+  /* Ajustement de l'espace entre les items */
+}
 </style>
